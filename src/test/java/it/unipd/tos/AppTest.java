e408ed2c98c27a38cb974adc002da0f317d2a97e
ToldoDM <bertoldo.damiano@gmail.com>
2020-12-05 17:25:09 +0100

    Modificate classi per testare chechstyle e junit

--- src/test/java/it/unipd/tos/AppTest.java
+++ src/test/java/it/unipd/tos/AppTest.java
@@ -1,38 +1,25 @@
+////////////////////////////////////////////////////////////////////
+// [DAMIANO] [BERTOLDO] [1193081]
+////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+	@Test 
     public void testApp()
     {
-        assertTrue( true );
+		App app = new App();
+		int sum = app.sum(1, 1);
+        assertEquals(2, sum);
     }
 }
